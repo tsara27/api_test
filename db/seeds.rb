356104b27d0c5240741c69c99a65fd6b0afd7a1a
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-09-24 21:33:30 +0700

    add dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Person.count == 0
+	Person.create([{ name: 'Andi', birthdate: '01/01/2014'}, { name: 'Budi', birthdate: '02/02/2014'}, { name: 'Charlie', birthdate: '03/03/2014'}, { name: 'Dede', birthdate: '06/06/2014'}])
+end
+
+if Device.count == 0
+	Device.create([{ os: 'apple iOS'}, { os: 'blackberry'}, { os: 'android'}, { os: 'feature phone'}])
+end
