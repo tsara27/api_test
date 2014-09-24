4a08d285134fc47b5f4b4edef3a8b599dec5884d
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-09-25 00:50:58 +0700

    add no content response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+	rescue_from ActiveRecord::RecordNotFound, :with => :respond_zero
+
+  def respond_zero
+  	render json: { errors: 'record not found'}
+  end
+
 end
