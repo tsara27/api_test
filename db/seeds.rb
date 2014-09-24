28721d92e7fe8e1620e1502c7dc23ffe7c7d62dd
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-09-25 00:51:25 +0700

    refactor seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-if Person.count == 0
-	Person.create([{ name: 'Andi', birthdate: '01/01/2014'}, { name: 'Budi', birthdate: '02/02/2014'}, { name: 'Charlie', birthdate: '03/03/2014'}, { name: 'Dede', birthdate: '06/06/2014'}])
-end
+Person.create([{ name: 'Andi', birthdate: '01/01/2014'}, { name: 'Budi', birthdate: '02/02/2014'}, { name: 'Charlie', birthdate: '03/03/2014'}, { name: 'Dede', birthdate: '06/06/2014'}]) if Person.count == 0
 
-if Device.count == 0
-	Device.create([{ os: 'apple iOS'}, { os: 'blackberry'}, { os: 'android'}, { os: 'feature phone'}])
-end
+Device.create([{ os: 'apple iOS'}, { os: 'blackberry'}, { os: 'android'}, { os: 'feature phone'}]) if Device.count == 0
