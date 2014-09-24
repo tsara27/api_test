e0aa2535875558291a555ace2d8587ce1c2c9943
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-09-24 21:32:59 +0700

    add devise model

--- db/migrate/20140924141806_create_devices.rb
+++ db/migrate/20140924141806_create_devices.rb
@@ -1,7 +1,7 @@
 class CreateDevices < ActiveRecord::Migration
   def change
-    create_table :devices do |t|
-
+    create_table :devices, :id => false do |t|
+      t.string :os
       t.timestamps null: false
     end
   end
