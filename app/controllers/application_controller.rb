7377dd066a29f9132a2661bb4fbd8d4bf3abfddd
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-09-25 01:25:50 +0700

    fix respond to zero

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
 	rescue_from ActiveRecord::RecordNotFound, :with => :respond_zero
 
   def respond_zero
-  	render json: { errors: 'record not found'}
+  	render json: { errors: 'record not found'}, status: :not_found
   end
 
 end
